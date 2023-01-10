333e7e9d958210f23e76e13609f854bf8f704ea4
jaragon78 <juan.ignacio.aragon@gmail.com>
2023-01-10 10:34:54 -0300

    Se actualiza backend

--- src/main/java/com/argentinaPrograma/Portfolio/WebConfig.java
+++ src/main/java/com/argentinaPrograma/Portfolio/WebConfig.java
@@ -1,9 +1,13 @@
 package com.argentinaPrograma.Portfolio;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 /**
@@ -12,7 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
  * @author YOProgramo
 
- */
+
 
 @EnableWebMvc
 
@@ -30,4 +34,35 @@ public class WebConfig implements WebMvcConfigurer {
 
    
 
+}
+
+@Configuration
+@ComponentScan(basePackageClasses = WebConfig.class, useDefaultFilters = false, includeFilters = {
+        @Filter(org.springframework.stereotype.Controller.class) })
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true).maxAge(3600);
+    }
+
+}
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
