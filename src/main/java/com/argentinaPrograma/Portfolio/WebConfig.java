06ee1401b21c0c9fdc731bfcfaa3d20b5994cf3a
jaragon78 <juan.ignacio.aragon@gmail.com>
2023-01-10 12:34:13 -0300

    Se actualiza backend

--- src/main/java/com/argentinaPrograma/Portfolio/WebConfig.java
+++ src/main/java/com/argentinaPrograma/Portfolio/WebConfig.java
@@ -1,74 +1,34 @@
-/*
+
 package com.argentinaPrograma.Portfolio;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-
-/**
-
- * Clase que habilita CORS
-
- * @author YOProgramo
-
-
 
 @EnableWebMvc
-
 @Configuration
-
 public class WebConfig implements WebMvcConfigurer {
 
+@Value("${allowed.origins}")
+private String allowedOrigins;
+@Value("${allowed.headers}")
+private String allowedHeaders;
+
     @Override
-
     public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**");
-
-    }
-
    
-
-}
-
-@Configuration
-@ComponentScan(basePackageClasses = WebConfig.class, useDefaultFilters = false, includeFilters = {
-        @Filter(org.springframework.stereotype.Controller.class) })
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
-                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-                        "Access-Control-Request-Headers")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowCredentials(true).maxAge(3600);
+    CorsRegistration reg = registry.addMapping("/**").allowedMethods("*");
+    reg.allowedOrigins(allowedOrigins.split(","));
+    reg.allowedHeaders(allowedHeaders.split(","));
     }
+ 
+}    
 
-}
 
-@Configuration
-public class WebConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
-                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-                        "Access-Control-Request-Headers")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowCredentials(true).maxAge(3600);
-    }
-        };
-    }
-}
-*  */
